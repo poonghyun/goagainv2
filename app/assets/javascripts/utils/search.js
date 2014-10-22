4c38151870e0ce2f470218aae64a3a3c5621902f
poonghyun <michael.ph.lee.12@gmail.com>
2014-10-21 17:35:45 -0700

    improve typeahead search

--- app/assets/javascripts/utils/search.js
+++ app/assets/javascripts/utils/search.js
@@ -47,26 +47,40 @@ $.BusinessSearch = function (el) {
         }
       })
 
-      $search = $('#typeahead');
+      ////
+
+      var $search = $('#typeahead');
+      var selected = 0;
+      var suggestions = 0;
+      var selectorString = "";
+      var b_id = 0;
 
       $search.keyup(function(e) {
         if(e.keyCode === 13) {
 
-          var suggestions = $('.tt-suggestion').length;
-
-          if($search.typeahead('val') && (suggestions > 0)) {
-            var b_id = $('.tt-suggestion > .business-search-result').data('b-id');
+          if($search.typeahead('val') && suggestions) {
             var fragmentUrl = "/business/" + b_id;
             Backbone.history.navigate(fragmentUrl, { trigger: true });
 
             $search.typeahead('val', '');
           }
         } else {
-          // handle non-enter input
-
-          // case for arrow keys
-          // case for typing
-
+          if(e.keyCode === 40) {
+            selected += 1;
+            if(selected > suggestions) selected = 0;
+            selectorString = '.tt-suggestion:nth-child(' + selected + ') .business-search-result';
+            b_id = $(selectorString).data('b-id');
+          } else if(e.keyCode === 38) {
+            selected -= 1;
+            if(selected < 0) selected = suggestions;
+            selectorString = '.tt-suggestion:nth-child(' + selected + ') .business-search-result';
+            b_id = $(selectorString).data('b-id');
+          } else if(e.keyCode > 64 && e.keyCode < 91) {
+            selected = 0;
+            suggestions = $('.tt-suggestion').length;
+            selectorString = '.tt-suggestion:nth-child(' + (selected + 1) + ') .business-search-result';
+            b_id = $(selectorString).data('b-id');
+          }
         }
       })
 
